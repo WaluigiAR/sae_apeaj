903d184099eb9685258536153ca28b556c86f895
mael <goujonmael@gmail.com>
2023-10-28 17:27:11 +0200

    Auth with DB

---
+++ src/main/java/com/webapp/ytb/webappytp/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.webapp.ytb.webappytp.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import com.webapp.ytb.webappytp.modele.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    User findUserByEmail(@Param("email") String email);
+}
