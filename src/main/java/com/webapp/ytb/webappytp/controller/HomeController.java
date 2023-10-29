854ec203020d70875d5294ada73363090cd8df9b
mael <goujonmael@gmail.com>
2023-10-29 20:42:15 +0100

    Display who's connected

--- src/main/java/com/webapp/ytb/webappytp/controller/HomeController.java
+++ src/main/java/com/webapp/ytb/webappytp/controller/HomeController.java
@@ -1,24 +1,28 @@
 package com.webapp.ytb.webappytp.controller;
 
-
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("")
 public class HomeController {
 
-    @ResponseBody
-    @RequestMapping(value = "/",method = RequestMethod.GET)
-    public String index(){
-        return "hello world";
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String redirectToAccueil() {
+        return "redirect:/accueil";
     }
 
-    @ResponseBody
-    @RequestMapping(value = "/about",method = RequestMethod.GET)
-    public String about(){
-        return "about";
+    @RequestMapping(value = "/accueil", method = RequestMethod.GET)
+    public String accueil(Model model) {
+        // Récupérez le nom de l'utilisateur connecté
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof User) {
+            String username = ((User) principal).getUsername();
+            model.addAttribute("username", username);
+        }
+        return "index";
     }
 }
