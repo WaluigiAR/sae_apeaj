f2f62000b97d653a154aeb195308262f410089d4
mael <goujonmael@gmail.com>
2023-10-31 22:38:55 +0100

    dynamic menu page

--- src/main/java/com/webapp/ytb/webappytp/controller/HomeController.java
+++ src/main/java/com/webapp/ytb/webappytp/controller/HomeController.java
@@ -1,6 +1,6 @@
 package com.webapp.ytb.webappytp.controller;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,16 +11,33 @@ public class HomeController {
 
     @GetMapping("/")
     public String redirectToAccueil() {
-        return "redirect:/accueil";
+        return "redirect:/menu";
     }
 
-
-    @GetMapping("/accueil")
-    public String yourPage(Model model, @AuthenticationPrincipal UserDetails userDetails) {
+    @GetMapping("/fiche")
+    public String fiche(Model model, @AuthenticationPrincipal UserDetails userDetails) {
         String username = userDetails.getUsername();
         String role = userDetails.getAuthorities().stream().findFirst().get().getAuthority();
         model.addAttribute("username", username);
         model.addAttribute("role", role);
         return "index";
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model, @AuthenticationPrincipal UserDetails userDetails) {
+        String role = userDetails.getAuthorities().stream().findFirst().get().getAuthority();
+        if ("ROLE_ADMIN".equals(role)) {
+            return "admin";
+        }
+        return "redirect:/menu";
+    }
+
+    @GetMapping("/menu")
+    public String menu(Model model, @AuthenticationPrincipal UserDetails userDetails) {
+        String username = userDetails.getUsername();
+        String role = userDetails.getAuthorities().stream().findFirst().get().getAuthority();
+        model.addAttribute("username", username);
+        model.addAttribute("role", role);
+        return "menu";
     }
 }
